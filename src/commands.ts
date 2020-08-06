47506b81c8a5746d581800ea36960b2b7e7ec156
Ananda Raj <araj2@atlassian.com>
2020-08-06 16:29:08 +0530

    add support to prompt the user for component path

--- src/commands.ts
+++ src/commands.ts
@@ -1,7 +1,32 @@
 import { cosmiconfigSync } from 'cosmiconfig';
-import { workspace as Workspace, window as Window, commands as Commands } from 'vscode';
+import * as fs from 'fs';
+import { workspace as Workspace, window as Window, WorkspaceFolder, QuickPickItem } from 'vscode';
 
 import { COSMICONFIG_MODULE_NAME } from './constants';
+
+interface WorkspaceFolderItem extends QuickPickItem {
+  folder: WorkspaceFolder;
+}
+
+const pickFolder = async (
+  folders: ReadonlyArray<WorkspaceFolder>,
+  placeHolder: string
+): Promise<WorkspaceFolder | undefined> => {
+  if (folders.length === 1) {
+    return Promise.resolve(folders[0]);
+  }
+
+  const selected = await Window.showQuickPick(
+    folders.map<WorkspaceFolderItem>((folder) => {
+      return { label: folder.name, description: folder.uri.fsPath, folder: folder };
+    }),
+    { placeHolder: placeHolder }
+  );
+  if (selected === undefined) {
+    return undefined;
+  }
+  return selected.folder;
+};
 
 export const createDefaultConfiguration = (): void => {
   const folders = Workspace.workspaceFolders;
@@ -25,4 +50,33 @@ export const createDefaultConfiguration = (): void => {
     }
     return;
   }
+  pickFolder(noConfigFolders, 'Select a workspace folder to generate a Snypet configuration for').then(
+    async (folder) => {
+      if (!folder) {
+        return;
+      }
+      let config = {
+        componentPath: '',
+        prefix: '',
+      };
+
+      const componentPath = await Window.showInputBox({
+        prompt: 'Select your React component folder',
+      });
+
+      if (componentPath) {
+        config = {
+          ...config,
+          componentPath,
+        };
+      }
+      const folderRootPath = folder.uri.fsPath;
+      const fileData = JSON.stringify(config, null, 2);
+      fs.writeFile(`${folderRootPath}/.snypetrc.json`, fileData, (err) => {
+        if (!err) {
+          Window.showInformationMessage(`Successfully created a snypet configuration for ${folder.name}`);
+        }
+      });
+    }
+  );
 };
